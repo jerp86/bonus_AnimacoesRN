749108071393b5e4f224079c3cf61183b133170f
jerp86 <jerp4@hotmail.com>
2019-11-24 09:33:11 -0300

    Animação timing

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View, Animated, StyleSheet } from 'react-native';
+
+// Animated.View
+// Animated.Text
+// Animated.Image
+// Animated.ScdrollView
+
+export default class App extends Component {
+  state = {
+    ballY: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.ball, { top: this.state.ballY }]} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#7159C1',
+  },
+});
